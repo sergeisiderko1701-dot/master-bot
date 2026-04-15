a8fa09cf7c7bdfccf94f2133e83ddc4e18791ebe
sergeisiderko1701-dot <sergei.siderko1701@gmail.com>
2026-04-16 00:06:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,5 +16,6 @@ class Settings:
     auto_apply_schema: bool = os.getenv("AUTO_APPLY_SCHEMA", "false").lower() == "true"
     
     client_order_cooldown: int = int(os.getenv("CLIENT_ORDER_COOLDOWN", "60"))
-
+    max_active_client_orders: int = int(os.getenv("MAX_ACTIVE_CLIENT_ORDERS", "3"))
+    
 settings = Settings()
